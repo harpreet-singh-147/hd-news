fe362a47440874e24c0e4e6876104d5edec863eb
harpreet-singh-147 <h_singh_007@hotmail.co.uk>
2022-12-08 15:39:25 +0000

    added navbar sign in modal

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,72 +1,88 @@
 import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
+import UserLoginModal from "./UserLoginModal";
 import "./Nav.css";
 
 const Nav = ({ loggedInUser, setLoggedInUser }) => {
   const [navOpen, setNavOpen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
   const location = useLocation();
 
   return (
-    <nav className="navbar">
-      <div className="container">
-        <div className="nav">
-          <input type="checkbox" id="nav-check" checked={navOpen} readOnly />
-          <div className="nav-header">
-            <div className="nav-title">
-              <Link onClick={() => setNavOpen(false)} to="/">
-                HD News
-              </Link>
-            </div>
-          </div>
-          <div
-            onClick={() => setNavOpen(!navOpen)}
-            className={navOpen ? "nav-btn open" : "nav-btn "}
-          >
-            <label htmlFor="nav-check">
-              <span></span>
-              <span></span>
-              <span></span>
-            </label>
-          </div>
+    <>
+      <nav className="navbar">
+        <div className="container">
+          <div className="nav">
+            <input type="checkbox" id="nav-check" checked={navOpen} readOnly />
 
-          <div className="nav-links">
-            {!loggedInUser ? (
-              <Link to="/articles" onClick={() => setNavOpen(false)}>
-                Articles
-              </Link>
-            ) : null}
-            {loggedInUser ? (
-              <>
-                <Link
-                  style={{
-                    pointerEvents: "none",
-                  }}
-                >
-                  Hello {loggedInUser.name.split(" ")[0]}
+            <div className="nav-header">
+              <div className="nav-title">
+                <Link onClick={() => setNavOpen(false)} to="/">
+                  HD News
                 </Link>
+              </div>
+            </div>
+            <div
+              onClick={() => setNavOpen(!navOpen)}
+              className={navOpen ? "nav-btn open" : "nav-btn "}
+            >
+              <label htmlFor="nav-check">
+                <span></span>
+                <span></span>
+                <span></span>
+              </label>
+            </div>
+
+            <div className="nav-links">
+              {!loggedInUser ? (
                 <Link to="/articles" onClick={() => setNavOpen(false)}>
                   Articles
                 </Link>
+              ) : null}
+              {loggedInUser ? (
+                <>
+                  <Link
+                    style={{
+                      pointerEvents: "none",
+                    }}
+                  >
+                    Hello {loggedInUser.name.split(" ")[0]}
+                  </Link>
+                  <Link to="/articles" onClick={() => setNavOpen(false)}>
+                    Articles
+                  </Link>
 
+                  <Link
+                    onClick={() => {
+                      setNavOpen(false);
+                      setLoggedInUser(null);
+                    }}
+                  >
+                    Sign out
+                  </Link>
+                </>
+              ) : !loggedInUser && location.pathname !== "/" ? (
                 <Link
-                  to="/"
                   onClick={() => {
                     setNavOpen(false);
-                    setLoggedInUser(null);
+                    setOpenModal(true);
                   }}
                 >
-                  Sign out
+                  Sign In
                 </Link>
-              </>
-            ) : !loggedInUser && location.pathname !== "/" ? (
-              <Link to="/" onClick={() => setNavOpen(false)}>
-                Sign In
-              </Link>
-            ) : null}
+              ) : null}
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      {openModal ? (
+        <UserLoginModal
+          setLoggedInUser={setLoggedInUser}
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+        />
+      ) : null}
+    </>
   );
 };
 
